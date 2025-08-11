990aa6fd2b3248dede00aecf92483e8387cb703e
helmutsteiner <helmut.steiner.development@gmail.com>
2025-08-11 15:39:23 +0200

    Adds frontend framework for simpler user input and defining parameters.

--- pkg/analog/graph.go
+++
@@ -1,7 +0,0 @@
-package analog
-
-// Graph represents a graph structure with vertices and edges for optimization problems.
-type Graph struct {
-	Vertices int      // Number of vertices in the graph
-	Edges    [][2]int // List of edges, where each edge is a pair of vertex indices
-}
